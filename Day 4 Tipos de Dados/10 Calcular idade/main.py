f5c0996ff9c0ad1659c2e77c87d6d8cec4d93b8f
Nathan Vasconcellos Vicente <nathanvvasconcellos09@gmail.com>
2026-08-13 21:36:51 -0300

    Corrige nome da pasta Calcular idade

---
+++ Day 4 Tipos de Dados/10 Calcular idade/main.py
@@ -0,0 +1,9 @@
+print("Olá! Seja bem-vindo! 👋")
+print("Vamos descobrir sua idade com base no ano em que você nasceu.\n")
+
+ano = int(input("Em que ano você nasceu? "))
+
+ano_atual = 2026
+idade = ano_atual - ano
+
+print(f"\nVocê tem ou vai fazer {idade} anos este ano! 🎉")
